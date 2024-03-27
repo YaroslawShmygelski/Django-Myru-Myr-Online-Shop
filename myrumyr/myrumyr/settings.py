3838c606c56af18a63d588c3e650936f17f3660c
Yaroslav <yaroslawsh04@gmail.com>
2024-03-27 23:55:06 +0100

    Db changed for PostgreSql

--- myrumyr/myrumyr/settings.py
+++ myrumyr/myrumyr/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'myrumyr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myrumyr_db',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
