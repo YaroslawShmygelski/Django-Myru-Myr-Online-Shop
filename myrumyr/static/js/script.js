7211f064d1276d2857db8843ce26728878b257f4
Yaroslav <yaroslawsh04@gmail.com>
2024-05-01 19:29:57 +0200

    Finished JS aspect of pop-up modal window behaviour || Added stylings || Now every product in catalog has it own independent modal window

--- myrumyr/static/js/script.js
+++ myrumyr/static/js/script.js
@@ -8,82 +8,80 @@ const dropdown_cat = document.getElementById("dropdown-cat");
 
 // Mobile side bar opener
 if (bar) {
-  bar.addEventListener("click", () => {
-    nav.classList.add("active-bar");
-  });
+    bar.addEventListener("click", () => {
+        nav.classList.add("active-bar");
+    });
 }
 
 if (close) {
-  close.addEventListener("click", (event) => {
-    nav.classList.remove("active-bar");
-  });
+    close.addEventListener("click", (event) => {
+        nav.classList.remove("active-bar");
+    });
 }
 
 // Search bar opener
 if (search_result) {
-  search_result.addEventListener("input", () => {
-    dropdown_menu.style.display = "inline";
-  });
+    search_result.addEventListener("input", () => {
+        dropdown_menu.style.display = "inline";
+    });
 }
 
 document.addEventListener("click", (event) => {
-  if (!dropdown_menu.contains(event.target) && event.target !== search_result) {
-    dropdown_menu.style.display = "none";
-  }
+    if (!dropdown_menu.contains(event.target) && event.target !== search_result) {
+        dropdown_menu.style.display = "none";
+    }
 });
-
 
 
 var MainImg = document.getElementById("main-image");
 var SmallImg = document.getElementsByClassName("small-img");
 // Realization of changing of images in row
 for (let i = 0; i < SmallImg.length; i++) {
-  SmallImg[i].onclick = function () {
-    MainImg.src = SmallImg[i].src;
-  };
+    SmallImg[i].onclick = function () {
+        MainImg.src = SmallImg[i].src;
+    };
 }
+
 function toggleIcon() {
-  var icon = $(".nav-catalog i");
-  icon.toggleClass("fa-chevron-down fa-chevron-up");
+    var icon = $(".nav-catalog i");
+    icon.toggleClass("fa-chevron-down fa-chevron-up");
 }
 
-// Open modal box
-document
-  .getElementById("open-modal-btn").addEventListener("click", function (event) {
-    document.getElementById("my-modal").classList.add("open");
-  });
 
-var innerLinks = document.querySelectorAll('.open-modal-btn');
+// Getting open-button collection for iteration
+var modalBtns = document.getElementsByClassName("openModalBtn");
 
-innerLinks.forEach(function(link) {
-  link.addEventListener('click', function(event) {
-    event.preventDefault(); // Предотвращаем действие по умолчанию для внутренней ссылки
-    event.stopPropagation(); // Предотвращаем всплытие события
-    window.location.href = this.parentElement.href; // Переходим по адресу родительской ссылки
-  });
-});
+// Iterating through every modalButton and adding Event Listeners for each of them
+for (var i = 0; i < modalBtns.length; i++) {
+    modalBtns[i].onclick = function () {
+        var productId = this.getAttribute("data-product-id");
+        var modal = document.getElementById("productModal" + productId);
+        modal.style.display = "block";
 
+        //added listener to close modal window with ESC
+        window.addEventListener('keydown', (e) => {
+            if (e.key === "Escape") {
+                modal.style.display = "none";
+            }
+        });
+        // added Listener to close modal window if clicked aside
+        modal.addEventListener('click', event => {
+            if (event._isClickWithInModal) return;
+            modal.style.display = "none";
+        });
+    }
+}
 
-// close modal
-document
-  .getElementById("close-my-modal-btn").addEventListener("click", function () {
-    document.getElementById("my-modal").classList.remove("open");
-  });
+// Getting close-button collection for iteration
+var closeBtns = document.getElementsByClassName("close");
 
-// close modal window with esc
-window.addEventListener("keydown", (e) => {
-  if (e.key === "Escape") {
-    document.getElementById("my-modal").classList.remove("open");
-  }
-});
+// Iterating through every modalButton and adding Event Listeners for each of them
+for (var j = 0; j < closeBtns.length; j++) {
+    closeBtns[j].onclick = function () {
+        var productId = this.getAttribute("data-product-id");
+        var modal = document.getElementById("productModal" + productId);
+        modal.style.display = "none";
 
-// Close modal window with click outside of the box
-document
-  .querySelector("#my-modal .modal__box")
-  .addEventListener("click", (event) => {
-    event._isClickWithInModal = true;
-  });
-document.getElementById("my-modal").addEventListener("click", (event) => {
-  if (event._isClickWithInModal) return;
-  event.currentTarget.classList.remove("open");
-});
+    }
+}
+
